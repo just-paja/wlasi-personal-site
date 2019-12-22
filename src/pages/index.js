adc3153371f928e62ba96f5ed31da782842ad67f
just-paja <just.paja@gmail.com>
2019-12-22 15:29:57 +0100

    Lay down project base

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Spreading joy through cocktails around the world.</h1>
+    <h2>Travel. Drinks. Joy.</h2>
+  </div>
+)
